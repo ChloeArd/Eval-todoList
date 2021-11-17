74347067a6786a86714f5eb6d2baf61695720a5a
ChloeArd <chloe.ardoise@gmail.com>
2021-11-17 18:02:10 +0100

    Eval

--- assets/js/components/EditButton.js
+++ assets/js/components/EditButton.js
@@ -9,7 +9,46 @@ export const EditButton = function (parent, idName, table) {
         document.getElementById("edit" + i).addEventListener("click", function () {
             let x = this.id;
             let recupId = x.replace("edit", "");
+            let valueList = document.getElementById("valueList" + recupId);
+            valueList.innerHTML = "";
+            let inputEdit = document.createElement("input");
+            inputEdit.value = table[recupId];
+            inputEdit.classList = "inputEdit";
+            inputEdit.id = "inputEdit" + recupId;
+            valueList.appendChild(inputEdit);
 
+            // button ok for edit
+            let ok = document.createElement("button");
+            ok.id = "ok" + recupId;
+            ok.classList = "ok";
+            ok.innerHTML = "Ok"
+            valueList.appendChild(ok);
+
+            // button cancel edit
+            let cancel = document.createElement("button");
+            cancel.id = "cancel" + recupId;
+            cancel.classList = "cancel";
+            cancel.innerHTML = "Annuler";
+            valueList.appendChild(cancel);
+
+
+            // edit a list
+            document.getElementById("ok" + recupId).addEventListener("click", function () {
+                let value = document.getElementById("inputEdit" + recupId).value;
+                let valueList = document.getElementById("valueList" + recupId);
+                valueList.innerHTML = value;
+                table[recupId] = value;
+                document.getElementById("cancel" + recupId).remove();
+                document.getElementById("ok" + recupId).remove();
+            })
+
+            // cancel a edit list
+            document.getElementById("cancel" + recupId).addEventListener("click", function () {
+                let valueList = document.getElementById("valueList" + recupId);
+                valueList.innerHTML = table[recupId];
+                document.getElementById("cancel" + recupId).remove();
+                document.getElementById("ok" + recupId).remove();
+            })
         });
     }
 }
